b77963f0ccbd26870aeeceef96d9e21d4385f461
Meghana <234g1a0507@srit.ac.in>
2025-07-12 16:06:58 +0530

    hii

---
+++ DAY 6/jsdemo.js
@@ -0,0 +1,70 @@
+// console.log("Welcome to js demo!");
+// document.write("<h1>Welocme to js Demo!</h1>"); 
+// let x ;
+// console.log(x);
+// let a = null;
+// console.log(a)
+
+// let flowers = ["rose","lilly","tulip"];
+// console.log(typeof (flowers))
+
+// let person = {name: "John",age : 30, city:"New York"};
+// console.log(typeof person);
+
+//alert() // give the kind of output
+//prompt() // takes the input in box
+//confirm() // confirm dailog box
+
+// let name = prompt("Enter name : ");
+// alert(name);
+// confirm("are a human?") ? alert("Welcome!") : alert("Acess Denied");
+
+//let age = prompt("enter your age: ");
+//if (age >= 18) {
+ //   alert("your are eligible to vote. ");
+//}
+//else {
+  //  alert("you are not eligible to vote.");
+//}
+
+//let num = prompt("enter a number: ");
+//if(num>100){
+ //   alert("the  number is greater than 100.");
+//}
+//else if (num > 50){
+ //   alert("the  number is greater than 50 but less than or equal to 100.");
+//}
+//else if (num > 0){
+  //  alert("the  number is greater than 0 but less than or equal to 50.");
+//}
+//else {
+ //   alert("the number is less than or equal to 0.")
+//}
+
+//if (condition1){
+  //  if(condition2){
+  //      //code to be executed
+   // }
+//}
+//const age = prompt("Enter your age: ");
+//if(age>=18){
+  //  const count = prompt("Enter your blood count: ");
+    //if(count>=100000){
+      //  const hemo = prompt("Enter your hemoglobin in %: ");
+        //if (hemo >= 12) {
+          //  const  sugar = prompt("Enter your blood sugar level: ");
+        //}
+        //if (sugar >= 80 && sugar <= 120) {
+          //  alert("you are eligible to donate blood.");
+        //} else {
+          //  alert("you are not  eligible to donate blood due to high sugar level.");
+     //  }
+    //} else {
+      //  alert("youare not  eligible to donate blood due to low hemoglobin.");
+   // }
+    //else {
+     //    alert("you are not eligible to donate blood due to low blood count.");
+    //}
+//}
+
+
